9c6e9e817b030a274f7b77288d1a886c726885e4
Alexander Fuchsberger <8102991+fuchsberger@users.noreply.github.com>
2024-03-07 15:42:41 -0500

    fixed a path issue

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from lab.dirs import list_dir
-
 basedir = input('Enter a starting directory or file: ')
 
-# Call list_dir which lists directories and files recursively
+# PART 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+from lab.dirs import list_dir
 list_dir( basedir )
+# PART 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# PART 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# from lab.dirattrs import list_dir, FileStats
+# fstats = FileStats()
+# list_dir( basedir, fstats )
+# fstats.print_results()
+# PART 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
